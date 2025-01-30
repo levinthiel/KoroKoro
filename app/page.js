adc04ba049b4bdae212bb47cd9d099eabc40f6cd
levin <levinthiel@proton.me>
2025-01-30 09:13:17 +0100

    styling of the buttons on title screen and updated space arcadia logo

--- app/page.js
+++ app/page.js
@@ -10,18 +10,18 @@ export default function Home() {
       <Wrapper>
         <SplashScreen>
           <Image
-            src={"/spacearcadialogo.png"}
+            src={"/spacearcadialogo-pixel.png"}
             width={300}
-            height={300}
+            height={270}
             alt="space arcadia logo"
           />
           <p>presents</p>
         </SplashScreen>
         <TitleScreen>
         <Image
-            src={"/titlescreen-logo.png"}
-            width={300}
-            height={300}
+            src={"/titlescreen-logo-pixel.png"}
+            width={350}
+            height={350}
             alt="korokoro logo"
           />
           <Link href="./OpeningScenePage">
@@ -86,7 +86,8 @@ const TitleScreen = styled.div`
   width: 100%;
   height: 100%;
   opacity: 0;
-  background: url("/titlescreen-bg.png") center center;
+  background: url("/titlescreen-bg-pixel.png") center center no-repeat;
+  background-size: cover;
   position: relative;
   animation-name: example3;
   animation-duration: 1s;
@@ -113,31 +114,47 @@ const TitleScreen = styled.div`
 const StyledTitleScreenButton = styled.button `
   font-family: inherit;
   padding: 14px 22px;
-  border-radius: 8px;
   cursor: pointer;
   background: var(--koro-pink);
-  border: 2px solid var(--koro-pink-dark);
   transition: 100ms ease;
-  box-shadow: 5px 5px 0 0 var(--koro-pink-dark);
   color: var(--koro-black);
+  border-radius: 20px;
+  border-width: 4px;
+  border-color: var(--koro-pink-dark);
+  border-image-slice: 4;
+  border-image-width: 2;
+  border-image-outset: 0;
+  border-image-source: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12'%3E%3Cpath d='M2 2h2v2H2zM4 0h2v2H4zM10 4h2v2h-2zM0 4h2v2H0zM6 0h2v2H6zM8 2h2v2H8zM8 8h2v2H8zM6 10h2v2H6zM0 6h2v2H0zM10 6h2v2h-2zM4 10h2v2H4zM2 8h2v2H2z' fill='currentcolor' /%3E%3C/svg%3E");
 
   &:hover {
-    box-shadow: 2px 2px 0 0 var(--koro-pink-dark);
+    background: var(--koro-pink-dark);
+  }
+  &:focus {
+    background: var(--koro-pink-dark);
+    font-size: medium;
   }
 `;
 const StyledTitleScreenButton2 = styled.button `
   font-family: inherit;
   padding: 14px 22px;
-  border-radius: 8px;
+  margin-top: 10px ;
   cursor: pointer;
   background: var(--koro-blue);
-  border: 2px solid var(--koro-blue-dark);
   transition: 100ms ease;
-  box-shadow: 5px 5px 0 0 var(--koro-blue-dark);
   color: var(--koro-black);
-  margin-top: 15px;
+  border-radius: 20px;
+  border-width: 4px;
+  border-color: var(--koro-pink-dark);
+  border-image-slice: 4;
+  border-image-width: 2;
+  border-image-outset: 0;
+  border-image-source: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12'%3E%3Cpath d='M2 2h2v2H2zM4 0h2v2H4zM10 4h2v2h-2zM0 4h2v2H0zM6 0h2v2H6zM8 2h2v2H8zM8 8h2v2H8zM6 10h2v2H6zM0 6h2v2H0zM10 6h2v2h-2zM4 10h2v2H4zM2 8h2v2H2z' fill='currentcolor' /%3E%3C/svg%3E");
 
   &:hover {
-    box-shadow: 2px 2px 0 0 var(--koro-blue-dark);
+    background: var(--koro-blue-dark);
+  }
+  &:focus {
+    background: var(--koro-blue-dark);
+    font-size: medium;
   }
 `;
