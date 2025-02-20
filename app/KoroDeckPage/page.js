670f8a337cbdadca7416680878aa6e207f82262a
levin <levinthiel@proton.me>
2025-02-20 09:33:27 +0100

    reduce ui to basic structure

--- app/KoroDeckPage/page.js
+++ app/KoroDeckPage/page.js
@@ -7,11 +7,7 @@ export default function KoroDeckPage() {
   return (
     <Wrapper>
       <KoroDeck>
-        <ScreenShadow>
-          <ScreenBezel>
-            <Screen></Screen>
-          </ScreenBezel>
-        </ScreenShadow>
+        
       </KoroDeck>
       <LinkWrapper>
         <Link href=".">back to home</Link>
@@ -31,41 +27,11 @@ const Wrapper = styled.div`
   justify-content: center;
 `;
 const KoroDeck = styled.div`
-  background: radial-gradient(
-    circle at 33% 15%,
-    #fffdf8 1px,
-    #f9edf3 2%,
-    #89b0d1 40%,
-    #5898cd 63%,
-    #000000 100%
-  );
-  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
-  width: 450px;
-  min-width: 450px;
-  height: 600px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: -30px -30px 55px rgba(0, 0, 0, 0.5) inset;
-  border: 1px solid #000;
-`;
-const ScreenShadow = styled.div`
-  box-shadow: 0px 0px 8px rgba(255, 255, 255, 0.2);
-  border-radius: 15px;
-  padding: 15px;
-  background: rgba(255, 255, 255, 0.2);
-`;
-const ScreenBezel = styled.div`
-  background-color: #a8c64e;
-  border-radius: 15px;
-  box-shadow: 0 0 1px 3px rgba(10, 10, 10, 0.7);
-`;
-const Screen = styled.div`
-  display: block;
-  box-shadow: inset 3px 3px 3px rgba(0, 0, 0, 0.3);
-  width: 280px;
-  height: 280px;
-  border-radius: 15px;
+  background:#C1C3B7;
+  border-radius: 5px;
+  width: 320px;
+  min-width: 320px;
+  height: 568px;
 `;
 const LinkWrapper = styled.div`
   color: green;
