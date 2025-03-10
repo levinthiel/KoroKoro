45c6ccf9469aad9d78aa8e8637c855a996daddd9
Stephan Model <stephan.model@outlook.de>
2025-03-10 11:14:10 +0100

    feat: expand jsx template and styling of button row component

--- app/components/layout/ButtonRow.js
+++ app/components/layout/ButtonRow.js
@@ -5,16 +5,27 @@ const gameControlButtons = ["feed", "sleep", "enter"];
 
 export default function ButtonRow() {
   return (
-    <StyledButtonRow>
-      <li>
-        {gameControlButtons.map((button) => {
-          return <Button label={button} />;
-        })}
-      </li>
-    </StyledButtonRow>
+    <StyledButtonRowWrapper>
+      <StyledButtonRow>
+        <li>
+          {gameControlButtons.map((button) => {
+            return <Button label={button} />;
+          })}
+        </li>
+      </StyledButtonRow>
+    </StyledButtonRowWrapper>
   );
 }
 
+const StyledButtonRowWrapper = styled.div`
+  background-color: #7e7f94; // dark case grey
+  border: 2px solid #c1c3b7; // game boy grey
+`;
+
 const StyledButtonRow = styled.ul`
+  flex-flow: row nowrap;
+  align-items: center;
   margin: 0 auto;
+  background-color: var(--koro-black);
+  border-radius: 9;
 `;
