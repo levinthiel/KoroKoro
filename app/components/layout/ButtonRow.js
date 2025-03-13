2abd2b78da11db90bd97342d02769ee42221643b
Stephan Model <stephan.model@outlook.de>
2025-03-13 19:05:00 +0100

    feat: improve jsx template, add styling

--- app/components/layout/ButtonRow.js
+++ app/components/layout/ButtonRow.js
@@ -1,28 +1,40 @@
 import styled from "styled-components";
 import Button from "../ui/Button.js";
 
-const gameControlButtons = ["feed", "sleep", "enter"];
+const gameControlButtons = [
+  { id: 1, label: "feed" },
+  { id: 2, label: "sleep" },
+  { id: 3, label: "enter" },
+];
 
 export default function ButtonRow() {
   return (
     <StyledButtonRowWrapper>
       <StyledButtonRow>
-        <li>
-          {gameControlButtons.map((button) => {
-            return <Button label={button} />;
-          })}
-        </li>
+        {gameControlButtons.map((button) => {
+          return <Button key={button.id} label={button.label} />;
+        })}
       </StyledButtonRow>
     </StyledButtonRowWrapper>
   );
 }
 
 const StyledButtonRowWrapper = styled.div`
-  background-color: #7e7f94; // dark case grey
+  // background-color: #7e7f94; // dark case grey
+
+  background-color: #c1c3b7;
   border: 2px solid #c1c3b7; // game boy grey
+  border-radius: 9px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 25%);
+  max-width: 199px;
+  max-height: 79px;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  margin-left: 18px;
 `;
 
-const StyledButtonRow = styled.ul`
+const StyledButtonRow = styled.div`
   flex-flow: row nowrap;
   align-items: center;
   margin: 0 auto;
