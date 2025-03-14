3eb2cacc2a979f2cb21b9d31c40f1bfe58f7ffa1
Stephan Model <stephan.model@outlook.de>
2025-03-14 21:39:55 +0100

    feat: add styling and additional styled-component to button to match hi-fi mockup
    I tried out gradient values diffrent to the one suggested in the mock up, I like these better, but happy to discuss how we want to go about this

--- app/components/layout/ButtonRow.js
+++ app/components/layout/ButtonRow.js
@@ -41,6 +41,6 @@ const StyledButtonRow = styled.div`
   display: flex;
   flex-wrap: nowrap;
   align-items: center;
-  justify-content: center;
+  justify-content: space-evenly;
   gap: 1px;
 `;
--- app/components/ui/Button.js
+++ app/components/ui/Button.js
@@ -1,13 +1,40 @@
 import styled from "styled-components";
 
 export default function Button({ label }) {
-  return <StyledGameControlButton>{label}</StyledGameControlButton>;
+  return (
+    <StyledGameControlButton>
+      <StyledButtonProfile>{label}</StyledButtonProfile>
+    </StyledGameControlButton>
+  );
 }
 
 const StyledGameControlButton = styled.button`
-  background-color: var(--case-grey);
+  background: linear-gradient(
+    to top,
+    var(--case-grey-dark) 0%,
+    var(--case-grey) 70%
+  );
   border: 1px solid var(--case-grey);
   border-radius: 5px;
   width: 55px;
   height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledButtonProfile = styled.div`
+  background: linear-gradient(
+    to right,
+    var(--case-grey-dark) 0%,
+    var(--case-grey) 85%
+  );
+  box-shadow: -1px 0px 1px #c1c3b7;
+  border: 1px solid var(--case-grey);
+  border-radius: 6px;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
